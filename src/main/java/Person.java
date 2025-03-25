5a1e9a410891ea72894bc373aa9c9c540dca1e3d
mrkotyaka <mrkotyaka@gmail.com>
2025-03-25 19:41:15 +0300

    release

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,4 +1,91 @@
-package PACKAGE_NAME;
+import java.util.Objects;
+import java.util.OptionalInt;
 
 public class Person {
+    protected final String name;
+    protected final String surname;
+    protected int age;
+    protected String address;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean hasAge() {
+        return getAge().isPresent();
+    }
+
+    public boolean hasAddress() {
+        if (address == null) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return OptionalInt.of(age);
+    }
+
+    public String getAddress() {
+        if (address == null) {
+            return "";
+        }
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        age++;
+    }
+
+    @Override
+    public String toString() {
+        String agePart = "";
+        String addressPart = "";
+        if (hasAge() && age != 0) {
+            agePart = ", " + getAge().getAsInt() + " г.";
+        }
+        if (hasAddress()) {
+            addressPart = ", живет в: " + getAddress();
+        }
+        return getName() + " " + getSurname() + agePart + addressPart;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.surname);
+        hash = 97 * hash + this.age;
+        hash = 97 * hash + Objects.hashCode(this.address);
+        return hash;
+    }
+
+    public PersonBuilder newChildBuilder() {
+        try {
+            return new PersonBuilder()
+                    .setSurname(this.getSurname())
+                    .setAddress(this.getAddress());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/PersonBuilder.java
+++ src/main/java/PersonBuilder.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class PersonBuilder {
+
+    protected String name;
+    protected String surname;
+    protected int age;
+    protected String address;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
+    public PersonBuilder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public Person build() {
+        if (name == null) {
+            throw new IllegalStateException("Не указано имя!");
+        }
+        if (surname == null) {
+            throw new IllegalStateException("Не указана фамилия!");
+        }
+        if (age < 0) {
+            throw new IllegalStateException("Возраст не может быть отрицательным!");
+        }
+
+        Person person = null;
+        if (age != 0) {
+            person = new Person(name, surname, age);
+        } else {
+            person = new Person(name, surname);
+        }
+        person.setAddress(address);
+        return person;
+    }
 }
